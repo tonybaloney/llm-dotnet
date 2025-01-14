8aa91128f64fc1c56a92439dd57f851f02c54947
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-01-14 17:00:52 +1100

    Update CSnakes and catch the non-streaming APIs

--- llm-dotnet/python/llm_wrapper.py
+++ llm-dotnet/python/llm_wrapper.py
@@ -3,7 +3,10 @@
 
 def prompt(model: str, prompt: str) -> str:
     """Call the LLM with the given prompt and return the response."""
-    return str(llm.get_model(model).prompt(prompt))
+    model = llm.get_model(model)
+    can_stream = model.can_stream  # Some models can't stream
+
+    return model.prompt(prompt, stream=can_stream).text()
 
 
 def get_models() -> list[str]:
